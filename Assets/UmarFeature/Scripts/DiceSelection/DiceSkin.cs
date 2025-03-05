8f3576c1edd792e01cab7152d9b4fdf7c5f8de7d
MUmar21 <96631305+MUmar21@users.noreply.github.com>
2025-03-05 18:02:40 +0500

    Dice Skin Selection implementation

--- Assets/UmarFeature/Scripts/DiceSelection/DiceSkin.cs
+++ Assets/UmarFeature/Scripts/DiceSelection/DiceSkin.cs
@@ -1,7 +1,20 @@
+using TMPro;
 using UnityEngine;
 
 public class DiceSkin : MonoBehaviour
 {
+    public int DiceNumber;
     public bool isUnlocked;
     public bool isSelected;
+    public Material Material;
+    public TextMeshProUGUI SelectedText;
+    public GameObject DiceLock;
+
+    private void OnEnable()
+    {
+        if(DiceNumber > 2)
+        {
+            isUnlocked = (PlayerPrefs.GetInt("UnlockedDice") == DiceNumber) ? true : false;
+        }
+    }
 }
